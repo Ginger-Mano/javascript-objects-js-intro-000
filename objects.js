451659115104ec0a7062d4eb29055c574c36506b
imperative-publisher-3811 <mcasimirian@gmail.com>
2020-04-28 15:30:26 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,6 +2,6 @@ var playlist = { artist: "song titles"}
 
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist.artistName = { artistName: "songTitle" };
+  playlist[artistName] = "songTitle";
   return playlist
 }
